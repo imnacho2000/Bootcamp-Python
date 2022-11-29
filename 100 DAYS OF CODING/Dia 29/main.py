11aa7362fc3cf7354f663df930181522bf899a6a
Ignacio Martin Carrera <78453702+imnacho2000@users.noreply.github.com>
2022-11-29 14:27:54 -0300

    Update main.py

--- 100 DAYS OF CODING/Dia 29/main.py
+++ 100 DAYS OF CODING/Dia 29/main.py
@@ -5,7 +5,8 @@ import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generar_contraseña():
-
+    label_text_password_entry.delete(0,END)
+    
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
