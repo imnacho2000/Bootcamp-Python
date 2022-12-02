e29f18e80aa609d30c85e441f390c2bfa9d8eb65
Ignacio Martin Carrera <78453702+imnacho2000@users.noreply.github.com>
2022-12-02 16:07:12 -0300

    Tengo que seguir

--- 100 DAYS OF CODING/Dia 31/main.py
+++ 100 DAYS OF CODING/Dia 31/main.py
@@ -5,16 +5,25 @@ from tkinter import*
 
 
 window = Tk()
-window.config(pady=50,padx=50,bg=BACKGROUND_COLOR)
+window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 
 image_cardback = PhotoImage(file=f"./images/card_back.png")
 image_front = PhotoImage(file=f"./images/card_front.png")
 image_rigth = PhotoImage(file=f"./images/right.png")
 image_wrong = PhotoImage(file=f"./images/wrong.png")
 
-canvas = Canvas(width=800,height=526,highlightthickness=0)
-canvas.create_image(200,325,image=image_cardback)
-canvas.grid(column=0,row=0)
+canvas = Canvas(width=800,height=526,highlightthickness=0,bg=BACKGROUND_COLOR)
+canvas.create_image(400,263,image=image_front)
+canvas.create_text(400,150,text="Hola",font=("Ariel",40,"italic"))
+canvas.create_text(400,263,text="Hola",font=("Ariel",60,"bold"))
+canvas.grid(column=0,row=0,columnspan=2)
+
+
+canvas_rigth = Button(image=image_rigth, highlightthickness=0,bg=BACKGROUND_COLOR)
+canvas_rigth.grid(column=1,row=1)
+
+canvas_wrong = Button(image=image_wrong, highlightthickness=0,bg=BACKGROUND_COLOR)
+canvas_wrong.grid(column=0,row=1)
 
 
 window.mainloop()
